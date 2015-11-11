289a9b422caae63f9001bf92e14745978473751e
drafteee <sergej_-96@mail.ru>
2015-11-11 21:07:07 +0300

    Залил файлы

---
+++ Лаба 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainCodeTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ButtonOfProceed = new System.Windows.Forms.Button();
+            this.FinalResultsTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem1});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem1
+            // 
+            resources.ApplyResources(this.fileToolStripMenuItem1, "fileToolStripMenuItem1");
+            this.fileToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem1});
+            this.fileToolStripMenuItem1.Name = "fileToolStripMenuItem1";
+            // 
+            // openFileToolStripMenuItem1
+            // 
+            resources.ApplyResources(this.openFileToolStripMenuItem1, "openFileToolStripMenuItem1");
+            this.openFileToolStripMenuItem1.Name = "openFileToolStripMenuItem1";
+            this.openFileToolStripMenuItem1.Click += new System.EventHandler(this.openFileToolStripMenuItem1_Click);
+            // 
+            // MainCodeTextBox
+            // 
+            resources.ApplyResources(this.MainCodeTextBox, "MainCodeTextBox");
+            this.MainCodeTextBox.Name = "MainCodeTextBox";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            resources.ApplyResources(this.openFileDialog1, "openFileDialog1");
+            // 
+            // ButtonOfProceed
+            // 
+            resources.ApplyResources(this.ButtonOfProceed, "ButtonOfProceed");
+            this.ButtonOfProceed.Name = "ButtonOfProceed";
+            this.ButtonOfProceed.UseVisualStyleBackColor = true;
+            this.ButtonOfProceed.Click += new System.EventHandler(this.ButtonOfProceed_Click);
+            // 
+            // FinalResultsTextBox
+            // 
+            resources.ApplyResources(this.FinalResultsTextBox, "FinalResultsTextBox");
+            this.FinalResultsTextBox.Name = "FinalResultsTextBox";
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.FinalResultsTextBox);
+            this.Controls.Add(this.ButtonOfProceed);
+            this.Controls.Add(this.MainCodeTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox MainCodeTextBox;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button ButtonOfProceed;
+        private System.Windows.Forms.TextBox FinalResultsTextBox;
+    }
+}
+
---
+++ Лаба 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class MainForm : Form
+    {
+        string DataOfMainFile;
+        string LineWithCode = "", NewDataOfMainFileWithoutComments = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        string DoReadStringOfNewDataOfMainFileWithoutComments(ref int LengthOfStringOfDataOfMainFile, ref int AdditionalValueForCountSymbolsInString, ref int LengthOfDataOfMainFile)
+        {
+            LineWithCode = "";
+
+            while ((LengthOfStringOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length) && (NewDataOfMainFileWithoutComments[LengthOfStringOfDataOfMainFile] != '\n'))
+            {
+                LineWithCode += Convert.ToString(NewDataOfMainFileWithoutComments[LengthOfStringOfDataOfMainFile]);
+                LengthOfStringOfDataOfMainFile++;
+                AdditionalValueForCountSymbolsInString++;
+            }
+
+            LengthOfDataOfMainFile += AdditionalValueForCountSymbolsInString + 1;
+            LengthOfStringOfDataOfMainFile++;
+            AdditionalValueForCountSymbolsInString = 0;
+
+            return LineWithCode;
+        }
+        void DoDeleteComments()
+        {
+            bool FlagOfStringComment = false;
+
+            for (int i = 0; i < DataOfMainFile.Length; i++)
+            {
+                if ((DataOfMainFile[i] == '/') && (DataOfMainFile[i + 1] == '/'))
+                    while (DataOfMainFile[i] != '\r')
+                    {
+                        i++;
+                    }
+
+                if ((DataOfMainFile[i] == '/') && (DataOfMainFile[i + 1] == '*'))
+                {
+                    while (FlagOfStringComment == false)
+                    {
+                        if ((DataOfMainFile[i] == '*') && (DataOfMainFile[i + 1] == '/'))
+                            FlagOfStringComment = true;
+                        i++;
+                    }
+                    i += 2;
+                    FlagOfStringComment = false;
+                }
+                NewDataOfMainFileWithoutComments += Convert.ToString(DataOfMainFile[i]);
+            }
+        }
+        void DoFindBlockOfFunctions(int CounterOfPositionInCode, ref int[,] ArrayOfPositionsProcedures, ref int CountOfProcedures, int LengthOfDataOfMainFile, int LengthOfStringOfDataOfMainFile)
+        {
+            int CountOfBracket = 0, AdditionalValueForCountSymbolsInString=0, StartPositionInStringOfCode=0, LengthOfStringOfCodeForCycle = 0, FinishPositionInStringOfCode=0;
+            bool FLagOfBracket = false, FLagOfBlock = true;
+
+            while (LengthOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length)
+            {
+                while (FLagOfBlock)
+                {
+                    if (LengthOfStringOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length)
+                    {
+                        LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+
+                        CounterOfPositionInCode++;
+                        LengthOfStringOfCodeForCycle = LineWithCode.Length;
+
+                        for (int i = 1; i < LengthOfStringOfCodeForCycle; i++)
+                        {
+                            if ((LineWithCode[i - 1] == ')') && (LineWithCode[i] == '\r'))
+                            {
+                                FLagOfBlock = true;
+                                FLagOfBracket = true;
+                                StartPositionInStringOfCode = CounterOfPositionInCode;
+                                ArrayOfPositionsProcedures[CountOfProcedures, 0] = StartPositionInStringOfCode;
+                            }
+                            else
+                                FLagOfBlock = false;
+                        }
+                    }
+
+                    LineWithCode = "";
+
+                    while (((LengthOfDataOfMainFile != NewDataOfMainFileWithoutComments.Length) && (FLagOfBracket == true)))
+                    {
+                        if (LengthOfStringOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length)
+                        {
+                            LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+
+                            CounterOfPositionInCode++;
+                            LengthOfStringOfCodeForCycle = LineWithCode.Length;
+
+                            for (int i = 0; i < LengthOfStringOfCodeForCycle; i++)
+                            {
+                                if (Convert.ToChar(LineWithCode[i]) == '{')
+                                    CountOfBracket++;
+                                if (Convert.ToChar(LineWithCode[i]) == '}')
+                                    CountOfBracket--;
+                            }
+
+                            LineWithCode = "";
+
+                            if (CountOfBracket == 0)
+                            {
+                                FLagOfBracket = false;
+                                FinishPositionInStringOfCode = CounterOfPositionInCode;
+                                ArrayOfPositionsProcedures[CountOfProcedures, 1] = FinishPositionInStringOfCode;
+                                CountOfProcedures++;
+                            }
+                        }
+                    }
+                    if (LengthOfDataOfMainFile >= NewDataOfMainFileWithoutComments.Length)
+                        FLagOfBlock = false;
+                    else
+                        FLagOfBlock = true;
+                }
+            }
+        }
+
+        void DoFindGlobalValues(int LengthOfDataOfMainFile, int LengthOfStringOfDataOfMainFile, ref int CountOfGlobalVariable)
+        {
+            int LengthOfStringOfLineWithCode = 0, AdditionalValueForCountSymbolsInString=0;
+            string StringOfPUBLICSTATIC = "public static";
+
+            while (LengthOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length)
+            {
+                LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+
+                LengthOfStringOfLineWithCode = LineWithCode.Length;
+
+                if (LineWithCode.IndexOf(StringOfPUBLICSTATIC) != -1)
+                {
+                    if (LineWithCode[LengthOfStringOfLineWithCode - 2] == ';')
+                        CountOfGlobalVariable++;
+                    LineWithCode = "";
+                }
+                else
+                    LineWithCode = "";
+            }
+        }
+
+        int DoViewAppelVariable(int Aup, int[,] ArrayOfPositionsProcedures, int CountOfProcedures)
+        {
+            int LengthOfStringOfLineWithCode = 0, StartPositionInStringOfCode = 0, FinishPositionInStringOfCode = 0, CounterOfFunctions = 0,
+                k = 0, LengthOfStringOfDataOfMainFile = 0, AdditionalValueForCountSymbolsInString = 0, LengthOfDataOfMainFile = 0;
+            string NameOfVariable = "";
+            bool FlagOfReallyAppealToVariable = true;
+
+            Aup = 0;
+            LengthOfStringOfLineWithCode = LineWithCode.Length;
+
+            for (int i = LengthOfStringOfLineWithCode - 1; i > 0; i--)
+            {
+                if (LineWithCode[i] == '=')
+                {
+                    FinishPositionInStringOfCode = i - 2;
+                    break;
+                }
+            }
+            for (int i = FinishPositionInStringOfCode; i > 0; i--)
+            {
+                if (LineWithCode[i] == ' ')
+                {
+                    StartPositionInStringOfCode = i + 1;
+                    break;
+                }
+            }
+
+            int LengthOfVariable = FinishPositionInStringOfCode - StartPositionInStringOfCode + 2;
+
+            for (int i = 0; i < LengthOfVariable; i++)
+            {
+                if (LineWithCode[StartPositionInStringOfCode + i] == ' ')
+                    break;
+                NameOfVariable += LineWithCode[StartPositionInStringOfCode + i];
+            }
+
+            while (CounterOfFunctions < CountOfProcedures)
+            {
+                FlagOfReallyAppealToVariable = true;
+
+                for (k = 0; k < ArrayOfPositionsProcedures[CounterOfFunctions, 0]; k++)
+                {
+                    LineWithCode = "";
+                    LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+                }
+
+                while ((LengthOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length) && (k < ArrayOfPositionsProcedures[CounterOfFunctions, 1]) && (FlagOfReallyAppealToVariable == true))
+                {
+                    if (LineWithCode.IndexOf(NameOfVariable) != -1)
+                    {
+                        Aup++;
+                        FlagOfReallyAppealToVariable = false;
+                    }
+
+                    LineWithCode = "";
+                    k++;
+
+                    LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+                }
+
+                LengthOfDataOfMainFile = 0;
+                LengthOfStringOfDataOfMainFile = 0;
+                CounterOfFunctions++;
+            }
+            return Aup;
+        }
+        int DoFindCountOfAppealToGlobalVariable(int Aup, ref int[,] ArrayOfPositionsProcedures, int CountOfProcedures, int CountOfGlobalVariable, int CounterOfPositionInCode, int LengthOfDataOfMainFile, int LengthOfStringOfDataOfMainFile)
+        {
+            int CountOfViewedVariables = 0, LengthOfStringOfLineWithCode = 0, AdditionalValueForCountSymbolsInString=0;
+            bool FlagOfTheLastPlaceGlobalVariable = true;
+            string StringOfPUBLICSTATIC = "public static";
+
+            while ((LengthOfDataOfMainFile < NewDataOfMainFileWithoutComments.Length) && (CountOfGlobalVariable > CountOfViewedVariables))
+            {
+
+                if (FlagOfTheLastPlaceGlobalVariable == true)
+                {
+                    for (int i = 0; i < CounterOfPositionInCode; i++)
+                    {
+                        LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+                        LineWithCode = "";
+                    }
+                }
+
+                FlagOfTheLastPlaceGlobalVariable = false;
+
+                LineWithCode = DoReadStringOfNewDataOfMainFileWithoutComments(ref LengthOfStringOfDataOfMainFile, ref AdditionalValueForCountSymbolsInString, ref LengthOfDataOfMainFile);
+
+                CounterOfPositionInCode++;
+                LengthOfStringOfLineWithCode = LineWithCode.Length;
+
+                if (LineWithCode.IndexOf(StringOfPUBLICSTATIC) != -1)
+                {
+                    if (LineWithCode[LengthOfStringOfLineWithCode - 2] == ';')
+                    {
+                        CountOfViewedVariables++;
+                        Aup += DoViewAppelVariable(Aup, ArrayOfPositionsProcedures, CountOfProcedures);
+                        FlagOfTheLastPlaceGlobalVariable = true;
+                        LengthOfStringOfDataOfMainFile = 0;
+                        LengthOfDataOfMainFile = 0;
+                    }
+                }
+                LineWithCode = "";
+            }
+            return Aup;
+        }
+
+        private void openFileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MainCodeTextBox.Text = "";
+                FinalResultsTextBox.Text = "";
+                NewDataOfMainFileWithoutComments = "";
+                DataOfMainFile = "";
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName, Encoding.Default);
+                DataOfMainFile = File.ReadAllText((openFileDialog1.FileName));
+
+                MainCodeTextBox.Text = sr.ReadToEnd();
+                sr.Close();
+
+                if (DataOfMainFile.Length != 0)
+                    ButtonOfProceed.Enabled = true;
+            }
+        }
+
+        private void ButtonOfProceed_Click(object sender, EventArgs e)
+        {
+
+            int[,] ArrayOfPositionsProcedures = new int[100, 2];
+            int CountOfGlobalVariable = 0,  CounterOfPositionInCode = 0, CountOfProcedures = 0, LengthOfDataOfMainFile = 0, LengthOfStringOfDataOfMainFile = 0;
+            int Aup = 0, Pup=0;
+            float Rup = 0;
+            ButtonOfProceed.Enabled = false;
+            DoDeleteComments();
+
+            DoFindBlockOfFunctions(CounterOfPositionInCode, ref ArrayOfPositionsProcedures, ref CountOfProcedures, LengthOfDataOfMainFile, LengthOfStringOfDataOfMainFile);
+
+            DoFindGlobalValues(LengthOfDataOfMainFile, LengthOfStringOfDataOfMainFile, ref CountOfGlobalVariable);
+
+            Aup = DoFindCountOfAppealToGlobalVariable(Aup, ref ArrayOfPositionsProcedures, CountOfProcedures, CountOfGlobalVariable, CounterOfPositionInCode, LengthOfDataOfMainFile, LengthOfStringOfDataOfMainFile);
+
+            Pup = CountOfGlobalVariable * CountOfProcedures;
+            Rup = (float)Aup / (float)Pup;
+
+            FinalResultsTextBox.Text = "The count global variable =" + Convert.ToString(CountOfGlobalVariable) + '\r' + '\n';
+            FinalResultsTextBox.Text += "The value Aup = " + Convert.ToString(Aup) + '\r' + '\n';
+            FinalResultsTextBox.Text += "The value Pup = " + Convert.ToString(Pup) + '\r' + '\n';
+            FinalResultsTextBox.Text += "The value Rup = Aup/Pup = " + Convert.ToString(Rup) + '\r' + '\n';
+        }
+    }
+}
